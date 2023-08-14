bef21105273d392980b136b2b5b5f3dc24640249
Julia Vitória Silva dos Santos <141461332+juliavitoriasilva20@users.noreply.github.com>
2023-08-14 19:25:32 -0300

    primeira versão programa placar do jogo

---
+++ placar.py
@@ -0,0 +1,10 @@
+time1 = int(input("quantos gols o primeiro time fez:")
+time2 = int(input("quantos gols o segundo time fez:")
+if time1 > time2:
+   print ("time 1 venceu !!!")
+
+if time2 > time1
+    print("time 2 venceu !!!")
+
+if time1 == time2:
+     print ("os times empataram")
